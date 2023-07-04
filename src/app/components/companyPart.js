76a2c171e78a9157bd0814051571804312cbbe5b
Bulygin Aleksey <alex987@bk.ru>
2023-07-04 21:32:21 +0300

    [feat]: Company part

--- src/app/components/companyPart.js
+++ src/app/components/companyPart.js
@@ -1,12 +1,15 @@
 import React from "react";
+//import { ReactComponent as CompanyLogo } from "../../assets/logos/company/8347700-ai.svg";
 
 const CompanyPart = () => {
   return (
     <div className="company-background">
+      {/* <CompanyLogo /> */}
       <img
-        className="profile__avatar"
-        src={require("../../assets/images/backgrounds/1-company.jpg")}
+        className="company-logo"
+        src={require("../../assets/logos/company/8347700-ai_cut.png")}
         alt=""
+        width="150px"
       />
       Company main info
     </div>
