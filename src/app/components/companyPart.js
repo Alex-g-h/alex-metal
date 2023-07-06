3872a9d8105873580a2b2b4e7581d712c46b34bc
Bulygin Aleksey <alex987@bk.ru>
2023-07-06 22:35:33 +0300

    [feat]: Header, footer. First view.

--- src/app/components/companyPart.js
+++ src/app/components/companyPart.js
@@ -1,17 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 //import { ReactComponent as CompanyLogo } from "../../assets/logos/company/8347700-ai.svg";
+import headerVideo from "../../assets/video/office.mp4";
 
 const CompanyPart = () => {
+  const isMuted = useRef(true);
+
+  // useEffect(() => {
+  // isMuted.current.defaultMuted = true;
+  // });
+
   return (
-    <div className="company-background">
+    // <div className="company-background">
+    <div>
       {/* <CompanyLogo /> */}
-      <img
+      {/* <img
         className="company-logo"
         src={require("../../assets/logos/company/8347700-ai_cut.png")}
         alt=""
         width="150px"
-      />
-      Company main info
+      /> */}
+      <div className="main-screen">
+        <header>
+          <a
+            href="index.html"
+            className="header_logo"
+          ></a>
+          <div className="header">
+            <div className="header_left"></div>
+            <div className="header_right">
+              <div className="header_phone">
+                <div>Офис</div>
+                <a href="tel:+74732224101">
+                  <span>+7 (473) 222-41-01</span>
+                </a>
+              </div>
+              <div className="header_phone">
+                <div>Склад</div>
+                <a href="tel:+74732224102">
+                  <span>+7 (473) 222-41-02</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="main_logo">
+            <div className="main_logo_img"></div>
+            <div className="main_logo_text">
+              <div className="main_logo_b">
+                Все виды
+                <br /> металлопроката
+              </div>
+              <div className="main_logo_s">от ведущих производителей</div>
+            </div>
+          </div>
+          <video
+            muted={isMuted.current}
+            // muted={true}
+            autoPlay
+            loop
+            // src="../../assets/video/header_fhd.mp4"
+            // poster="video/header_hd.jpg"
+          >
+            <source
+              src={headerVideo}
+              // src="../../assets/video/header_fhd.mp4"
+              // src="https://drive.google.com/file/d/1vm52ZqyBs53ga6OL0DCFg1nCrierbYrX/view"
+              type="video/mp4"
+            />
+            {/* <source
+              src="../../assets/video/Untitled.webm"
+              type="video/webm"
+            ></source> */}
+          </video>
+        </header>
+      </div>
     </div>
   );
 };
--- src/app/components/footerPart.js
+++ src/app/components/footerPart.js
@@ -1,7 +1,71 @@
 import React from "react";
 
 const FooterPart = () => {
-  return <div className="footer-background">Footer</div>;
+  return (
+    <div className="footer footer-background">
+      {/* // <div className="footer"> */}
+      <div className="container">
+        <div className="footer_left">
+          <div className="footer_logo">
+            {/* <div class="footer_logo_img"></div> */}
+            <img
+              className="company-logo"
+              src={require("../../assets/logos/company/8347700-ai_cut.png")}
+              alt=""
+              width="150px"
+            />
+            <div className="footer_logo_text">
+              <p>
+                Все виды металлопроката
+                <br /> от ведущих производителей
+              </p>
+              <p>
+                <b>СтальИнвест</b>
+              </p>
+            </div>
+          </div>
+          <div className="footer_copyright">
+            <span></span> 2023
+          </div>
+        </div>
+        <div className="footer_right">
+          <div className="footer_nav">
+            <nav>
+              <ul>
+                <li>
+                  <a href="catalog/index.htm">Продукция</a>
+                  <ul>
+                    <li>
+                      <a href="catalog/sortovoy-prokat/index.htm">
+                        Сортовой прокат
+                      </a>
+                    </li>
+                    <li>
+                      <a href="catalog/listovoy-prokat/index.htm">
+                        Листовой прокат
+                      </a>
+                    </li>
+                    <li>
+                      <a href="catalog/truby/index.htm">Трубы</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="standards/index.htm">Справочники и ГОСТы</a>
+                </li>
+                <li>
+                  <a href="documents/index.htm">Документы</a>
+                </li>
+                <li>
+                  <a href="rekvizity.php.html">Реквизиты</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FooterPart;
