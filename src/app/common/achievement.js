3d9649cab8b10b26f1673c136d1bbc5676321836
Bulygin Aleksey <alex987@bk.ru>
2023-07-08 02:51:52 +0300

    [feat]: Achievements

---
+++ src/app/common/achievement.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Achievement = ({ text, icon }) => {
+  return (
+    <div className="achievement">
+      <div className="achievement-icon">
+        <img
+          src={icon}
+          alt=""
+        />
+      </div>
+      <div className="achievement-text text_shadow">{text}</div>
+    </div>
+  );
+};
+
+export default Achievement;
--- src/app/components/achievementsPart.js
+++ src/app/components/achievementsPart.js
@@ -1,10 +1,25 @@
 import React from "react";
+import Achievement from "../common/achievement";
 
 const AchievementsPart = () => {
   return (
-    <div className="achievements-background">
-      Years on market, tonnes sold metal, # cities customers, # deliveries, #
-      cuts, etc.
+    <div className="achievements-background achievements">
+      <Achievement
+        text="15+ лет на рынке"
+        icon={require("../../assets/icons/time.svg").default}
+      />
+      <Achievement
+        text="2.5+ млн тонн отгружено"
+        icon={require("../../assets/icons/metal.svg").default}
+      />
+      <Achievement
+        text="В 20+ городов поставлено"
+        icon={require("../../assets/icons/city.svg").default}
+      />
+      <Achievement
+        text="45+ крупных заказчиков"
+        icon={require("../../assets/icons/customer.svg").default}
+      />
     </div>
   );
 };
