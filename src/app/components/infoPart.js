1d04cc9bb1f8f3301fe709ef4af9e9fcc385e93f
Bulygin Aleksey <alex987@bk.ru>
2023-07-08 01:39:58 +0300

    [feat]: Info

--- src/app/components/infoPart.js
+++ src/app/components/infoPart.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const InfoPart = () => {
-  return <div className="info-background">Short company info</div>;
+  return (
+    // <div className="info-background">
+    <div>
+      <div className="main_about main_about_flex">
+        <div className="container">
+          {/* <div className="main_about_order"> */}
+          <div className="main_about_title text_shadow">
+            ООО «СтальИнвест» — современная, развивающаяся компания. Является
+            активным участником на рынке поставок металлопроката в Черноземье и
+            Центральном регионе России.
+          </div>
+          <div className="main_about_text text_shadow">
+            {`Компания вышла на рынок стального проката в 2007 году, 
+            успешно преодолев с тех пор четыре экономических кризиса.`}
+          </div>
+          {/* </div> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InfoPart;
