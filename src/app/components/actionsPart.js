4bf6e76143ab5b02805aa498d2f61b9ddd81c438
Bulygin Aleksey <alex987@bk.ru>
2023-07-08 00:33:22 +0300

    [feat]: Actions

--- src/app/components/actionsPart.js
+++ src/app/components/actionsPart.js
@@ -1,7 +1,53 @@
 import React from "react";
 
 const ActionsPart = () => {
-  return <div className="actions-background">Metal, cut, delivery</div>;
+  return (
+    // <div className="actions-background">
+    <div>
+      <div className="main_wedo">
+        <div className="container">
+          <div className="main_wedo_row">
+            <div className="main_wedo_item">
+              <div className="main_wedo_img">
+                <img
+                  src={require("../../assets/images/backgrounds/2-actions-1.jpg")}
+                  alt=""
+                />
+              </div>
+              <div className="main_wedo_cont">
+                <div className="main_wedo_title">Металлопрокат</div>
+                <div className="main_wedo_text">оптом и в розницу</div>
+              </div>
+            </div>
+            <div className="main_wedo_item">
+              <div className="main_wedo_img">
+                <img
+                  src={require("../../assets/images/backgrounds/2-actions-2.jpg")}
+                  alt=""
+                />
+              </div>
+              <div className="main_wedo_cont">
+                <div className="main_wedo_title">Порезка, упаковка</div>
+                <div className="main_wedo_text">в необходимый размер</div>
+              </div>
+            </div>
+            <div className="main_wedo_item">
+              <div className="main_wedo_img">
+                <img
+                  src={require("../../assets/images/backgrounds/2-actions-3.jpg")}
+                  alt=""
+                />
+              </div>
+              <div className="main_wedo_cont">
+                <div className="main_wedo_title">Доставка</div>
+                <div className="main_wedo_text">по всей России и СНГ</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ActionsPart;
