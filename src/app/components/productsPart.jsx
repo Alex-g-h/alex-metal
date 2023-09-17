24237d04bd2066326d632f8cd4f733b3863bfd49
Bulygin Aleksey <alex987@bk.ru>
2023-09-17 20:36:48 +0300

    [fix]: Production part autosize

--- src/app/components/productsPart.jsx
+++ src/app/components/productsPart.jsx
@@ -20,23 +20,16 @@ const ProductsPart = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
-          initialSlide: 0,
         },
       },
       {
-        breakpoint: 480,
+        breakpoint: 670,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          initialSlide: 0,
         },
       },
     ],
